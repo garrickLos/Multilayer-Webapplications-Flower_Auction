f999d64cf7db75ca05c6fa6e70379b698252fb77
Garrick <76995865+garrickLos@users.noreply.github.com>
2025-10-31 16:10:26 +0100

    added privacy screen html, css and js file

--- front-end/webpagina's/javascript/itemFiller.js
+++ front-end/webpagina's/javascript/itemFiller.js
@@ -1,4 +1,4 @@
-async function genereerDataItem(containerId, divClass, jsonItem) {
+async function genereerDataItem(containerId, divClass, jsonItem, url) {
     const container = document.getElementById(containerId);
 
     if (!container) {
@@ -7,14 +7,14 @@ async function genereerDataItem(containerId, divClass, jsonItem) {
             return;
     }
 
-    const dealsArray = await dataOphalen(jsonItem);
+    const jsonArray = await dataOphalen(jsonItem, url);
 
-    if (!dealsArray || dealsArray.length === 0) {
+    if (!jsonArray || jsonArray.length === 0) {
         console.warn('Geen deals gevonden of er is een fout opgetreden bij het ophalen.');
         return;
     }
     
-    dealsArray.forEach(itemInfo => {          
+    jsonArray.forEach(itemInfo => {         
             const itemcontainer = document.createElement('div');
             itemcontainer.classList.add(divClass);  
             const ImagePath = itemInfo.imagePath || '/front-end/resources/pictures/webp/MissingPicture.webp';
@@ -32,12 +32,11 @@ async function genereerDataItem(containerId, divClass, jsonItem) {
         });
 }
 
-async function dataOphalen(jsonItem){
-    const url = '/front-end/dummydata/HoofdschermMock.json'; // Het pad naar je JSON-bestand
+async function dataOphalen(jsonItem, jsonurl){
+    const url = jsonurl; // Het pad naar je JSON-bestand
 
     try {
         const response = await fetch(url);
-
         
         if (!response.ok) {
             throw new Error(`Fout bij het ophalen: ${response.status} ${response.statusText}`);
