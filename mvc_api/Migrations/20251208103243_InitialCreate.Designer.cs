3454359eb3f7347625ea2f938b03f6edfe19e30d
BayramGurel <24031194@student.hhs.nl>
2025-12-08 11:35:12 +0100

    success merg with thomas

--- mvc_api/Migrations/20251208103243_InitialCreate.Designer.cs
+++ mvc_api/Migrations/20251208103243_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using mvc_api.Data;
 namespace mvc_api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20251208102708_InitialCreate")]
+    [Migration("20251208103243_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
