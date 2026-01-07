dc946f3be732f56120ac730a17c6e6f63875fbb1
Garrick <76995865+garrickLos@users.noreply.github.com>
2026-01-07 09:39:49 +0100

    Knoppen voor 404 pagina geupdate zodat het iets beter eruit ziet

--- front-end/react/src/schermen/404Scherm/404.tsx
+++ front-end/react/src/schermen/404Scherm/404.tsx
@@ -38,8 +38,15 @@ function sessieVerlopenError() {
         Probeer de pagina te refreshen of opnieuw in te loggen
       </span>
       <br></br>
+
       <GenereerKnop
-        classNames={'Button'} 
+        classNames={['Button', 'VerlopenSessie']} 
+        bericht={'Terug'} 
+        to={`/home`} 
+      />
+
+      <GenereerKnop
+        classNames={['Button', 'VerlopenSessie']} 
         bericht={'inloggen'} 
         to={`/inloggen`} 
       />
