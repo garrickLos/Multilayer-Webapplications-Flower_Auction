1ae6b17b201937db6d8b81e0ad5232d3671d996e
BayramGurel <24031194@student.hhs.nl>
2026-01-08 08:56:01 +0100

    nieuw migrations

--- Backend/mvc_api/Migrations/20260108074547_InitialCreate.Designer.cs
+++ Backend/mvc_api/Migrations/20260108074547_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using mvc_api.Data;
 namespace mvc_api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260107185814_InitialCreate")]
+    [Migration("20260108074547_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
